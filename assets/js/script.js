7cc2df28bb373c71d362bff3f5f4243922c53430
LawrenceArcher <archer@hey.com>
2022-04-17 18:04:09 +0100

    create slidehow for images on the homepage

--- assets/js/script.js
+++ assets/js/script.js
@@ -48,3 +48,41 @@ function toggleRelax() {
     document.getElementById("eat-toggle").classList.remove("active")
     document.getElementById("relax-toggle").classList.add("active")
 }
+
+// code for slideshow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    showNextSlides(slideIndex += n);
+}
+
+function showNextSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
